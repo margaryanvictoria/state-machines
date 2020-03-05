267ddb2b344883d55f6b2a4a0382c18ab790ddee
margaryanvictoria <margaryanvictoria@gmail.com>
2020-03-05 15:24:51 -0800

    SoDA practice for State Machines.

---
+++ Assets/Scripts/StateMachine.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class StateMachine : System.Object {
+    private Dictionary<string, System.Action<StateType>> states = new Dictionary<string, System.Action<StateType>>();
+    private string stateId = string.Empty;
+    private System.Action<StateType> current = null;
+
+    public string StateId {
+        get {
+            //return the stateId
+            return this.stateId;
+        } set {
+            //attempt to transition to the requested state
+            this.Transition(value);
+        }
+    }
+
+    protected virtual void Transition(string state) {
+        //if the state requested is null or empty, return
+        if (string.IsNullOrEmpty(state)) return;
+
+        //if the current state and the requested state are the same, return
+        if (string.Compare(this.StateId, state) == 0) return;
+
+        //if the requested state has not been registered with this state machine, return
+        if (this.states.ContainsKey(state) == false) return;
+
+        /*
+        if (string.IsNullOrEmpty(this.StateId) == false) {
+            //notify the current state that it is about to exit
+            this.states[this.StateId].Invoke(StateType.Exit);
+        } */
+
+        //notify the current state it is about to exist
+        this.current?.Invoke(StateType.Exit);
+
+        //lowercase or else we'll hit an infinite loop
+        //change the internal stateId to the requested /valid/ state
+        this.stateId = state;
+
+        //grab the current state
+        this.current = this.states[this.StateId];
+
+        //this.states[this.StateId].Invoke(StateType.Enter);
+        //notify the current state we are about to enter
+        this.current.Invoke(StateType.Enter);
+    }
+
+    public virtual void Add(string stateId, System.Action<StateType> state) {
+        if (this.states.ContainsKey(stateId) == false) {
+            this.states.Add(stateId, null);
+        }
+
+        this.states[stateId] = state;
+    }
+
+    public virtual void remove(string stateId) {
+        this.states.Remove(stateId);
+    }
+
+    public virtual void Update() {
+        /*
+        if (this.current == null) return;
+        this.current.Invoke(StateType.Update); */
+
+        //update the state machine
+        this.current?.Invoke(StateType.Update);
+    }
+
+    public enum StateType {
+        Unknown = 0,
+        Enter = 1,
+        Update = 2,
+        Exit = 3
+    }
+}
---
+++ Assets/Scripts/TrafficLight.cs
@@ -0,0 +1,166 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* New TrafficLight script using our generic StateMachine script. */
+public class TrafficLight : MonoBehaviour {
+    /* The amount of time each state will last. */
+	public float redStateTime = 5.0f;
+    public float yellowStateTime = 2.0f;
+    public float greenStateTime = 3.0f;
+
+	/* Materials. */
+	public MeshRenderer redMesh;
+	public MeshRenderer yellowMesh;
+	public MeshRenderer greenMesh;
+
+	private LightState state = LightState.None;
+    private float stateElapsedTime = 0.0f;
+
+	private StateMachine stateMachine = new StateMachine();
+	//private Dictionary<LightState, System.Action> states = new Dictionary<LightState, System.Action>();
+	private void Start() {
+		this.states.Add("Red", this.RedState);
+		this.states.Add("Yellow", this.YellowState);
+		this.states.Add("Green", this.GreenState);
+
+		this.stateMachine.StateId = "Red";
+		//this.TransitionState(LightState.Red);
+	}
+
+	void RedState() {
+		if(stateElapsedTime >= this.redStateTime) {
+			/*
+			this.state = LightState.Green;
+			this.stateElapsedTime = 0.0f; */
+			this.TransitionState(LightState.Green);
+			return;
+		}
+		/*
+		Color color = this.redMesh.material.color;
+		color.a = 1.0f;
+		this.redMesh.material.color = color;
+
+		color = yellowMesh.material.color;
+		color.a = .3f;
+		this.yellowMesh.material.color = color;
+		color = greenMesh.material.color;
+		color.a = .3f;
+		this.greenMesh.material.color = color; */
+
+		this.stateElapsedTime += Time.deltaTime;
+	}
+
+	void YellowState() {
+		if(this.stateElapsedTime >= this.yellowStateTime) {
+			this.TransitionState(LightState.Red);
+			return;
+		}
+
+		/*
+		Color color = this.yellowMesh.material.color;
+		color.a = 1.0f;
+		this.yellowMesh.material.color = color;
+
+		color = redMesh.material.color;
+		color.a = .3f;
+		this.redMesh.material.color = color;
+		color = greenMesh.material.color;
+		color.a = .3f;
+		this.greenMesh.material.color = color; */
+
+		this.stateElapsedTime += Time.deltaTime;
+	}
+
+	void GreenState() {
+		if (this.stateElapsedTime >= this.greenStateTime) {
+			this.TransitionState(LightState.Yellow);
+			return;
+		}
+
+		/*
+		Color color = this.redMesh.material.color;
+		color.a = 1.0f;
+		this.redMesh.material.color = color;
+
+		color = yellowMesh.material.color;
+		color.a = .3f;
+		this.yellowMesh.material.color = color;
+		color = redMesh.material.color;
+		color.a = .3f;
+		this.redMesh.material.color = color; */
+
+		this.stateElapsedTime += Time.deltaTime;
+	}
+
+	private void TransitionState(LightState next) {
+		this.state = next;
+		this.stateElapsedTime = 0.0f;
+
+		Color color;
+
+		switch (this.state) {
+			case LightState.Red:
+				color = this.redMesh.material.color;
+				color.a = 1.0f;
+				this.redMesh.material.color = color;
+
+				color = yellowMesh.material.color;
+				color.a = .3f;
+				this.yellowMesh.material.color = color;
+				color = greenMesh.material.color;
+				color.a = .3f;
+				this.greenMesh.material.color = color;
+				break;
+			case LightState.Yellow:
+				color = this.yellowMesh.material.color;
+				color.a = 1.0f;
+				this.yellowMesh.material.color = color;
+
+				color = redMesh.material.color;
+				color.a = .3f;
+				this.redMesh.material.color = color;
+				color = greenMesh.material.color;
+				color.a = .3f;
+				this.greenMesh.material.color = color;
+				break;
+			case LightState.Green:
+				color = this.redMesh.material.color;
+				color.a = 1.0f;
+				this.redMesh.material.color = color;
+
+				color = yellowMesh.material.color;
+				color.a = .3f;
+				this.yellowMesh.material.color = color;
+				color = redMesh.material.color;
+				color.a = .3f;
+				this.redMesh.material.color = color;
+				break;
+
+		}
+	}
+
+	void Update() {
+		/* By using a Dictionary we can avoid this:
+		switch (this.state) {
+			case LightState.Red:
+				this.RedState();
+				break;
+			case LightState.Yellow:
+				this.YellowState();
+				break;
+			case LightState.Green:
+				this.GreenState();
+				break;
+		} */
+		System.Action currentState = this.states[this.state];
+		currentState();
+	}
+}
+
+public enum LightState {
+	None = 0,
+	Red = 1,
+	Yellow = 2,
+	Green = 3
+}
